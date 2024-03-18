dbd47d6863e8e901253fccb076d7f0340ae43870
Kevin Li <lkevi@microsoft.com>
2024-03-18 11:13:39 +0800

    show exception detail

--- function_app.py
+++ function_app.py
@@ -1,3 +1,4 @@
+import os
 import azure.functions as func
 import datetime
 import json
@@ -12,28 +13,42 @@ app = func.FunctionApp()
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
+    # Check for MSI_SECRET environment variable
+    if 'MSI_SECRET' not in os.environ:
+        return func.HttpResponse(
+            "Managed Identity was not enabled.",
+            status_code=400
+        )
+
     KEY_VAULT_URL = "https://your-vault.vault.azure.net/"
 
-    credential = DefaultAzureCredential()
+    try:
+        credential = DefaultAzureCredential()
 
-    # Get secret from Azure Key Vault
-    secret_name = "test"
-    secret_client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
-    retrieved_secret = secret_client.get_secret(secret_name)
-    print(f"The secret value is: {retrieved_secret.value}")
+        # Get secret from Azure Key Vault
+        secret_name = "test"
+        secret_client = SecretClient(vault_url=KEY_VAULT_URL, credential=credential)
+        retrieved_secret = secret_client.get_secret(secret_name)
+        print(f"The secret value is: {retrieved_secret.value}")
 
-    # Similarily, get certificate from Azure Key Vault
-    # certificate_client = CertificateClient(vault_url=KEY_VAULT_URL, credential=credential)
-    # certificate_name = "test"
-    # certificate = certificate_client.get_certificate(certificate_name)
-    # print("Certificate Name:", certificate.name)
-    # print("Certificate Value:", certificate.cer)
+        # Similarily, get certificate from Azure Key Vault
+        # certificate_client = CertificateClient(vault_url=KEY_VAULT_URL, credential=credential)
+        # certificate_name = "test"
+        # certificate = certificate_client.get_certificate(certificate_name)
+        # print("Certificate Name:", certificate.name)
+        # print("Certificate Value:", certificate.cer)
 
-    # Return retrieved secret if available
-    if retrieved_secret and retrieved_secret.value:
-        return func.HttpResponse(f"Hello, The secret value is: {retrieved_secret.value}.")
-    else:
+        # Return retrieved secret if available
+        if retrieved_secret and retrieved_secret.value:
+            return func.HttpResponse(f"Hello, The secret value is: {retrieved_secret.value}.")
+        else:
+            return func.HttpResponse(
+                 "No value found",
+                 status_code=200
+            )
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
         return func.HttpResponse(
-             "No value found",
-             status_code=200
+            f"An error occurred: {str(e)}",
+            status_code=500
         )
